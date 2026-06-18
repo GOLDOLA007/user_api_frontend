f01554c112cd7ed03451562615395a10517f1194
Kaua <kauaoliveira18062007@gmail.com>
2026-06-17 23:09:13 -0300

    test

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 const urls = ['http://localhost:8080/users/status-javaconfig'];
 
-let i =0;
-
 setInterval(testAPI, 60000); // Call testAPI every 1min
-
+let i =0;
 function testAPI() {
     fetch(urls[0])
     .then(response => response.text())
         //.then(text => console.log(text))
-        .then(text => document.getElementById('response').textContent = text + " " + i)
+        .then(text => document.getElementById('response').textContent = text)
+        .then(text => document.getElementById('counter').textContent = 'API called (1min intervals): ' + i + ' times')
         .catch(error => document.getElementById('response').textContent = 'Error: ' + error);    
         i=i+1;
 }
